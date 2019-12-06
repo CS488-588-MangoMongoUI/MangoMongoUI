85978642bb3516f64acf488fd02ccf8b497e0753
Qiacheng Li <choliaky@gmail.com>
2019-12-05 21:31:11 -0800

    updated About comp

--- client/src/components/About.component.js
+++ client/src/components/About.component.js
@@ -5,12 +5,17 @@ export default class about extends Component{
   render(){
     var iframestyle ={
       width: '800px',
-      height: '1000px'
+      height: '1000px',
+      margin: 'auto'
+
+    }
+    var divstyle = {
+
     }
     return(
-      <div>
+        <p align='center'>
         <iframe style={iframestyle} src="https://docs.google.com/document/d/e/2PACX-1vTYM2SeLyJx9fJ4KY3ME9VegzLWE25M7OrqOaE9nbcHmoqPxx4zTECAOhk3KTSKtWlo8zX1GlilFgEj/pub?embedded=true"></iframe>
-      </div>
+        </p>
     )
   }
 }
